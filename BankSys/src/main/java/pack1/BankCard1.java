84fda9424f82f8757652a2df6392baa8a11e6f97
KateOl <kates13@rambler.ru>
2016-12-22 20:02:05 +0100

    Shuster lesson4

--- BankSys/src/main/java/pack1/BankCard1.java
+++ BankSys/src/main/java/pack1/BankCard1.java
@@ -1,5 +1,7 @@
+package pack1;
+
 /**
  * Created by eoleynik on 12/22/2016.
  */
-public class Pack1 {
+public class BankCard1 {
 }
---
+++ BankSys/src/main/java/pack1/Sum1.java
@@ -0,0 +1,15 @@
+package pack1;
+
+
+
+public class Sum1 {
+
+    public long sumMon;
+    public short currenCod;
+
+    public static void main(String[] args) {
+
+
+
+    } //main
+} // class
